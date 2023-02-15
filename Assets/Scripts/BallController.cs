c1278a0fc2ea5c5f2e99df25ebfbff674a0621ec
Oğuzhan Öztürk <oguzztrk1@gmail.com>
2023-02-15 16:35:58 +0300

    added new feature: ball's speed increases every round

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -6,27 +6,37 @@ public class BallController : MonoBehaviour
 {
     private Rigidbody2D ballRB;
     public float ballSpeed;
-    public bool hasRun = false;
+    public bool hasRun;
+    public float ballSpeedIncrease;
 
     // Start is called before the first frame update
+    private void Start() {
+        startGame(ballSpeed);
+    }
    private void Update() {
+    //start round if hasRun=false
     if(!hasRun){
         if(Input.GetKeyDown(KeyCode.Space)){
-            startGame();
+            ballSpeed += ballSpeedIncrease;
+            startGame(ballSpeed);
             hasRun = true;
         }
     }
    }
 
+    //reset ball position on beginning of every round.
     private void OnCollisionEnter2D(Collision2D other) {
-        if(other.gameObject.CompareTag("GoalPost")){
+        //when the ball touches goalpost or outofborder tagged gameobjects, reset position of the ball.
+        if(other.gameObject.CompareTag("GoalPost") || other.gameObject.CompareTag("OutOfBorders")){
             ballRB.velocity = new Vector2(0,0);
             this.gameObject.transform.position = new Vector2(0,0);
             hasRun = false;
         }
+        
     }
 
-    void startGame(){
+    //starting round fuction. Assign random direction to the ball.
+    void startGame(float ballSpeed){
         ballRB = this.GetComponent<Rigidbody2D>();
         float x = Random.Range(0, 2) == 0 ? -1 : 1;
         float y = Random.Range(0, 2) == 0 ? -1 : 1;
